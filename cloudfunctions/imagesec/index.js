6575ace61fb57f2af97008fb69a71d92f9da04e3
Zira <48037743+wasfzxt@users.noreply.github.com>
2021-08-02 09:29:23 +0800

    Update index.js

--- cloudfunctions/imagesec/index.js
+++ cloudfunctions/imagesec/index.js
@@ -1,5 +1,7 @@
 const cloud = require('wx-server-sdk');
-cloud.init()
+cloud.init({
+  env: cloud.DYNAMIC_CURRENT_ENV
+})
 
 exports.main = async (event, context) => {
   const imgmsg = (await cloud.downloadFile({
