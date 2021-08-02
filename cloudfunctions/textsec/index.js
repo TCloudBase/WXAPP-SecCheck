2343deefb8ebbfe27460fcabf578b754a31d5811
Zira <48037743+wasfzxt@users.noreply.github.com>
2021-08-02 09:29:36 +0800

    Update index.js

--- cloudfunctions/textsec/index.js
+++ cloudfunctions/textsec/index.js
@@ -1,5 +1,7 @@
 const cloud = require('wx-server-sdk')
-cloud.init()
+cloud.init({
+  env: cloud.DYNAMIC_CURRENT_ENV
+})
 
 exports.main = async (event, context) => {
   return await cloud.openapi.security.msgSecCheck({
